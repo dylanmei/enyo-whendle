d6389e61667567bdb0978a9ae7960f525a324413
dylanmei <dylanmei@gmail.com>
2011-10-23 21:14:43 -0700

    Add an 'about' dialog; refer to LICENSE.

---
+++ application/app/about.js
@@ -0,0 +1,45 @@
+(function() {
+  var dialog = {
+    kind: 'enyo.ModalDialog',
+    name: 'App.AboutDialog',
+    dismissWithEscape: true,
+    dismissWithClick: true,
+    components: [
+      { kind: 'HFlexBox', components: [
+        { kind: 'Image', src: '../icon-48.png' },
+        { flex:1, style: 'margin: 0 0 0 8px;', components: [
+          { name: 'name', className: 'name', content: '' },
+          { name: 'vendor', className: 'vendor', content: 'by Dylan Meissner / Hoop Engines' },
+        ]}
+      ]},
+      { kind: enyo.VFlexBox, autoVertical: false, components: [
+        { kind: enyo.Spacer },
+        { kind: 'HtmlContent', srcId: 'about', className: 'content', onLinkClick: 'on_link_clicked' },
+        { kind: enyo.Spacer }
+      ]}
+    ],
+    create: function() {
+      this.addClass('dialog');
+      this.addClass('about-dialog');
+      this.inherited(arguments);
+    },
+    open: function(alarm) {
+      this.setBoundsInfo("applyCenterBounds", []);
+      this.inherited(arguments);
+    },
+    afterOpen: function() {
+      this.inherited(arguments);
+      this.$.name.setContent('Whendle v' +
+        enyo.fetchAppInfo().version);
+    },
+    close: function(){
+      this.inherited(arguments);
+    },
+
+    on_link_clicked: function(sender, href) {
+      window.location(href);
+    }
+  };
+
+  enyo.kind(dialog);
+})();
